9bfbdcdf53be1d8d2f78b76f05db93c6b05f2492
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-04-01 18:07:37 -0400

    adds validations to Campsite model

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,4 +1,6 @@
 class Campsite < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :address, presence: true
     has_many :reviews
     has_many :users, through: :reviews
 end
