17e834a7aba54ffc7bfff1cc00b4497ee139a160
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-04-01 14:28:11 -0400

    adds user/new signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all
+    end
+    
     def new
         @user = User.new
     end
 
     def create
-
+        @user = User.new(user_params)
+        @user.save
+        redirect_to user_path(@user)
     end
     
     def show
-        @user = User.find_by(params[:id])
+        @user = User.find(params[:id])
     end
 
     private
