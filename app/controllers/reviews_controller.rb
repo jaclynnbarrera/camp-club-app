d57d56a8ad45c041913b41fcc4575f421eed22fc
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-04-01 10:56:43 -0400

    adds CRUD actions and strong params in ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,4 +6,31 @@ class ReviewsController < ApplicationController
     def index
         @reviews = Review.all
     end
+
+    def new
+
+    end
+
+    def create
+        @review = Review.new(review_params)
+        @review.save
+        redirect_to review_path(@review)
+    end
+
+    def edit 
+        @review = Review.find(params[:id])
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        @review.update(review_params)
+        redirect_to review_path(@review)
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:review, :rating, :user_id, :campsite_id)
+    end
+
 end
