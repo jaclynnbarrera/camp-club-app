e2fb5ccb16c32a31d7fcea832feb140bb200c668
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-04-01 21:41:24 -0400

    adds before_validation lifecycle method in campsite model

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,8 +1,14 @@
 class Campsite < ApplicationRecord
     validates :name, presence: true, uniqueness: true
-    validates :address, presence: true
-    validates :bio, presence: true
+    validates_presence_of :address, :bio
     #maybe add length validation?
     has_many :reviews
     has_many :users, through: :reviews
+    
+     before_validation :make_title_case
+
+     def make_title_case
+        self.name = self.name.titlecase
+    end
+    
 end  
