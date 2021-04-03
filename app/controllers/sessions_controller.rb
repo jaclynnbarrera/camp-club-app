285f06fe054f64234203033aa5780594ef22d588
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-04-03 16:01:58 -0400

    adds login action to SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController\
 
     def new
-        
+        # @user = User.new
     end
 
     def create
-        
+        @user = User.find_by(username: params[:user][:username])
+        #checking if user entered right password
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        elsif @user
+            @errors = ["Invalid Password"]
+            render :new
+        else
+            @errors = ["Invalid Username"]
+            render :new
+        end
     end
 
     def destroy
