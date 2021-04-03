2d5e3de1d2b8273b6dfc9ee19c9a6846c78c071d
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-04-02 21:07:43 -0400

    adds custom validation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
+    before_validation :make_title_case
+
     validates_presence_of :first_name, :last_name, :email
     validates_uniqueness_of :email
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
     has_many :reviews
     has_many :campsites, through: :reviews
+
+    def make_title_case
+        self.first_name = self.first_name.titlecase
+        self.last_name = self.last_name.titlecase
+    end
 
 end
