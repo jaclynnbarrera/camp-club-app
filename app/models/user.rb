9eee35e2120b5d7739035533d0692eceb093b8f4
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-04-02 20:37:31 -0400

    adds email validation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     validates_presence_of :first_name, :last_name, :email
     validates_uniqueness_of :email
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
     has_many :reviews
     has_many :campsites, through: :reviews
-end
 
- 
+end
