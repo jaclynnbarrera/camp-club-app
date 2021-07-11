cd4eb903a7e696911a62eee3e86e1dabb7f1b74a
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-07-11 00:11:03 -0400

    fix migration mispelling

--- db/migrate/20210404002838_removecolumnsfromusers.rb
+++ db/migrate/20210404002838_removecolumnsfromusers.rb
@@ -1,5 +1,5 @@
 class Removecolumnsfromusers < ActiveRecord::Migration[6.1]
   def change
-      remove_column :users, :google_toke, :string
+      remove_column :users, :google_token, :string
   end
 end
