b0b6637ff761932c8ebb2df532eeb0bc45c1548b
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-07-11 00:26:13 -0400

    code

--- db/migrate/20210404002545_delete_columns_from_users.rb
+++
@@ -1,6 +0,0 @@
-class DeleteColumnsFromUsers < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :users, :google_token, :string
-    remove_column :users, :google_refresh_token, :string
-  end
-end
