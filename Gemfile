d58b2d237439b77d2b6d03a2316baa4f9e5bec03
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-10-18 16:10:25 -0400

    sets up bootstrap

--- Gemfile
+++ Gemfile
@@ -23,12 +23,10 @@ gem 'webpacker', '~> 5.0'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
-
 gem 'bcrypt'
-
 gem 'pry'
-
 gem 'geocoder'
+gem 'bootstrap', '~> 5.0'
 
 #Bootstrap
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,11 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
+import "bootstrap";
+import "../stylesheets/application";
 
-require("stylesheets/application.scss");
+require("@popperjs/core");
+require("../stylesheets/application.scss");
 
 //= require jquery
 //= require bootstrap-sprockets
